936f2697c81b8aa4678e41c0e8b4c684e2de40b2
Ángel Barroso Sanz <angel.barroso@linguaserve.com>
2022-10-03 14:09:41 +0200

    Added basic test for Calculator component and tsconfig.json

---
+++ src/Calculator.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import { evaluate } from 'mathjs'
+
+export const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+export const rows = [[7, 8, 9], [4, 5, 6], [1, 2, 3], [0]]
+
+export const operations = ['+', '-', '*', '/']
+export const equalSign = '='
+
+export const Calculator = () => {
+  const [value, setValue] = useState('')
+
+  const createHandleClick = op => (): void =>
+    setValue(value.concat(op.toString()))
+  return (
+    <div>
+      <h1>Calculator</h1>
+      <input value={value} readOnly/>
+      <div role='grid'>
+        {rows.map((row, index) => (
+          <div key={index} role='row'>
+            {row.map(number => (
+              <button
+                onClick={createHandleClick(number)}
+                key={number}
+              >
+                {number}
+              </button>
+            ))}
+          </div>
+        ))}
+        {operations.map(operation => (
+          <button onClick={createHandleClick(operation)} key={operation}>{operation}</button>
+        ))}
+        <button onClick={(): void => setValue(evaluate(value))}>{equalSign}</button>
+      </div>
+    </div>
+  )
+}
---
+++ test/Calculator.test.tsx
@@ -0,0 +1,94 @@
+import { render, screen, cleanup, fireEvent } from '@testing-library/react'
+
+import { afterEach, describe, expect, it } from 'vitest'
+import { Calculator, operations, equalSign, numbers } from '../src/Calculator'
+
+
+
+describe('Calculator', (): void => {
+  afterEach(cleanup)
+  it('should be rendered', () => {
+    render(<Calculator />)
+  })
+  it('should render title correctly', (): void => {
+    render(<Calculator />)
+
+    screen.getByText('Calculator')
+  })
+  it('should render numbers', (): void => {
+    render(<Calculator />)
+    numbers.forEach(number => {
+      screen.getByText(number)
+    })
+  })
+  it('should render 4 rows', (): void => {
+    render(<Calculator />)
+    const rows = screen.getAllByRole('row')
+    expect(rows).toHaveLength(4)
+  })
+  it('should render operations', (): void => {
+    render(<Calculator />)
+    operations.forEach(operation => {
+      screen.getByText(operation)
+    })
+  })
+  it('should render equal sign', (): void => {
+    render(<Calculator />)
+    screen.getByText('=')
+  })
+  it('should render an input', (): void => {
+    render(<Calculator />)
+    screen.getByRole('textbox')
+  })
+  it('should user input after clicking a number', (): void => {
+    render(<Calculator />)
+    const one = screen.getByText('1')
+    fireEvent.click(one)
+    const input: HTMLInputElement = screen.getByRole('textbox')
+    expect(input.value).toBe('1')
+  })
+  it('should user input after clicking several numbers', (): void => {
+    render(<Calculator />)
+
+    const one = screen.getByText('1')
+    fireEvent.click(one)
+
+    const two = screen.getByText('2')
+    fireEvent.click(two)
+
+    const three = screen.getByText('3')
+    fireEvent.click(three)
+
+    const input: HTMLInputElement = screen.getByRole('textbox')
+    expect(input.value).toBe('123')
+  })
+  it('should show user inputs after clicking numbers and operations', (): void => {
+    render(<Calculator />)
+
+    const one = screen.getByText('1')
+    fireEvent.click(one)
+
+    const plus = screen.getByText('+')
+    fireEvent.click(plus)
+    fireEvent.click(one)
+
+    const input: HTMLInputElement = screen.getByRole('textbox')
+    expect(input.value).toBe('1+1')
+  })
+  it('should calculated based on user input and show the calculation', (): void => {
+    render(<Calculator />)
+
+    const one = screen.getByText('1')
+    fireEvent.click(one)
+
+    const plus = screen.getByText('+')
+    fireEvent.click(plus)
+    fireEvent.click(one)
+
+    const equal = screen.getByText(equalSign)
+    fireEvent.click(equal)
+
+    const input: HTMLInputElement = screen.getByRole('textbox')
+    expect(input.value).toBe('2')
+  })
+})
